3be9f5d7560d464d73f1bf4b01c7da1c91f6a912
Jim Chen <kinghotsysu@gmail.com>
2019-12-08 22:23:06 -0800

    fix present controller bug

--- WebkitViewController/Classes/WebkitViewController.swift
+++ WebkitViewController/Classes/WebkitViewController.swift
@@ -206,6 +206,7 @@ open class WebkitViewController: UIViewController, WebkitProtocol {
   // MARK: Life Cycle
   override open func viewDidLoad() {
     super.viewDidLoad()
+    definesPresentationContext = true
     progressView.frame = progressViewFrame()
     navigationController?.navigationBar.addSubview(progressView)
     if wasPresented() {
